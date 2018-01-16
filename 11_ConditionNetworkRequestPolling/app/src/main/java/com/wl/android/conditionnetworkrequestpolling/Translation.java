68d6d417525a31c4195591186443502cfdaa1604
unknown <D22397@SZCKT.COM>
2018-01-16 14:03:10 +0800

    有条件地网络请求轮询

---
+++ 11_ConditionNetworkRequestPolling/app/src/main/java/com/wl/android/conditionnetworkrequestpolling/Translation.java
@@ -0,0 +1,95 @@
+package com.wl.android.conditionnetworkrequestpolling;
+
+import android.util.Log;
+
+/**
+ * Created by D22397 on 2018/1/16.
+ */
+
+public class Translation {
+
+    /**
+     * status : 1
+     * content : {"from":"en-EU","to":"zh-CN","out":"示例","vendor":"ciba","err_no":0}
+     */
+
+    private int status;
+    private ContentBean content;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public ContentBean getContent() {
+        return content;
+    }
+
+    public void setContent(ContentBean content) {
+        this.content = content;
+    }
+
+    public static class ContentBean {
+        /**
+         * from : en-EU
+         * to : zh-CN
+         * out : 示例
+         * vendor : ciba
+         * err_no : 0
+         */
+
+        private String from;
+        private String to;
+        private String out;
+        private String vendor;
+        private int err_no;
+
+        public String getFrom() {
+            return from;
+        }
+
+        public void setFrom(String from) {
+            this.from = from;
+        }
+
+        public String getTo() {
+            return to;
+        }
+
+        public void setTo(String to) {
+            this.to = to;
+        }
+
+        public String getOut() {
+            return out;
+        }
+
+        public void setOut(String out) {
+            this.out = out;
+        }
+
+        public String getVendor() {
+            return vendor;
+        }
+
+        public void setVendor(String vendor) {
+            this.vendor = vendor;
+        }
+
+        public int getErr_no() {
+            return err_no;
+        }
+
+        public void setErr_no(int err_no) {
+            this.err_no = err_no;
+        }
+    }
+
+    //定义 输出返回数据 的方法
+    public void show() {
+        Log.d("RxJava", content.out );
+    }
+}
