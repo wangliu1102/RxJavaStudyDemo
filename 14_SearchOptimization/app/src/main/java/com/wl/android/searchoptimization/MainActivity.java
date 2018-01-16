c0fe6f0e2e6e238a151cf3bd1787f68508abb4eb
unknown <D22397@SZCKT.COM>
2018-01-16 16:33:02 +0800

    联想搜索优化

---
+++ 14_SearchOptimization/app/src/main/java/com/wl/android/searchoptimization/MainActivity.java
@@ -0,0 +1,63 @@
+package com.wl.android.searchoptimization;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "RxJava";
+    // 控件绑定
+    private EditText mEditText;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.ed);
+        mTextView = (TextView) findViewById(R.id.tv);
+
+        /*
+         * 说明
+         * 1. 此处采用了RxBinding：RxTextView.textChanges(name) = 对对控件数据变更进行监听（功能类似TextWatcher），
+         * 需要引入依赖：compile 'com.jakewharton.rxbinding2:rxbinding:2.0.0'
+         * 2. 传入EditText控件，输入字符时都会发送数据事件（此处不会马上发送，因为使用了debounce（））
+         * 3. 采用skip(1)原因：跳过 第1次请求 = 初始输入框的空字符状态
+         **/
+        RxTextView.textChanges(mEditText)
+                .debounce(1, TimeUnit.SECONDS) //每1秒中采用数据
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<CharSequence>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(CharSequence charSequence) {
+                        mTextView.setText("发送给服务器的字符 = " + charSequence.toString());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应");
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+                });
+    }
+}
