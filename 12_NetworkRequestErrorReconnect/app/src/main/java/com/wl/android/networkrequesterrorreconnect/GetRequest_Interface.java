efdb009660d41bf8a7146733da549168d4e31c13
unknown <D22397@SZCKT.COM>
2018-01-16 14:28:31 +0800

    网络请求出错重连

---
+++ 12_NetworkRequestErrorReconnect/app/src/main/java/com/wl/android/networkrequesterrorreconnect/GetRequest_Interface.java
@@ -0,0 +1,14 @@
+package com.wl.android.networkrequesterrorreconnect;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
+/**
+ * Created by D22397 on 2018/1/16.
+ */
+
+public interface GetRequest_Interface {
+
+    @GET("ajax.php?a=fy&f=auto&t=auto&w=hi%20world")
+    Observable<Translation> getCall();
+}
